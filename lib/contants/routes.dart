bfbc652a609366621864e759e965412ce44083a7
John Rey Balais <balaisjohnrey80@gmail.com>
2023-07-12 15:45:42 +0800

    step-13

--- lib/contants/routes.dart
+++ lib/contants/routes.dart
@@ -2,3 +2,4 @@ const registerRoute = '/register/';
 const loginRoute = '/login/';
 const notesRoute = '/notes/';
 const verifyEmailRoute = '/verify-email/';
+const newNoteRoute = '/notes/new-note/';
